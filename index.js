11580a65b6e726207d75e22150fc05a660870dcd
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-24 16:44:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 
 function myAnimal() {
-  let animal = "dog"
+  const animal = "dog"
   return animal
 }
 
 function yourAnimal() {
-  let animal = "cat" 
+  const animal = "cat" 
   return animal
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  two = 2
+  
 }
